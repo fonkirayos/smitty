043af74b89ce5f9b665042e4f1d32c40dce4b6b8
DESKTOP-MJMJB0F\frank <frankigps@hotmail.com>
2020-11-26 01:17:07 -0700

    Add project

---
+++ smitty/Project1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+//estos son comentarios 
+
+int main() {
+
+
+	//te voy a imprimir hola smith en la pantalla
+	std::cout << "Hola smith" << std::endl;
+
+
+
+
+	return 0; // regresamos 0 porque main tecnicamente es una función y las funciones dependiendo del tipo tienen que regresar el mismo valor
+	//tecnicamente cuando algo recibe de input 0 es porque no hay error
+}
